2ee4a522344b4b74ba4ef1eb331e53494d13f356
Chaka06 <issiakadiarrassouba501@gmail.com>
2025-11-04 17:52:45 +0000

    Update build script and fix DEBUG parsing

--- build.sh
+++ build.sh
@@ -5,9 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist (for collectstatic)
+mkdir -p static
+
 # Collect static files
 python manage.py collectstatic --no-input
 
 # Run migrations
-python manage.py migrate
-
+python manage.py migrate --no-input
